33728f820b8d94208d1ca6d0253710338645a36e
lolson5858 <109360413+lolson5858@users.noreply.github.com>
2022-08-10 12:27:21 -0500

    lines as arrays in object

--- apps.js
+++ apps.js
@@ -1,7 +1,15 @@
 const greeting = "Good Morning Luke! Your inspirational poem of the day is.....";
-const firstLines = ["Behold, a being of true heart!", "Today is a good day to be your best self", "Blessed be the fruit"];
-const secondLines = ["one that fears no failure,", "FEAR NOT", "like a blackberry glittering in the sun"]
-const thirdLines = ["you shall prevail.", "for you are the chosen one.", "go kick life's ass."]
+const poemOb = {
+  firstLines:["Behold, a being of true heart!", "Today is a good day to be your best self", "Blessed be the fruit"],
+  secondLines: ["one that fears no failure,", "FEAR NOT", "like a blackberry glittering in the sun"],
+  thirdLines: ["you shall prevail.", "for you are the chosen one.", "go kick life's ass."]
+}
 
+const mashup = () => {
+  let poem = ''
+  let firstLine = poemOb.firstLines[Math.floor(Math.random() * 3)]
+  console.log(firstLine)
+  
+}
 
-console.log(greeting);
+mashup()
