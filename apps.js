dd5eebad64d9cbe81fcb773c70e37203874e103e
lolson5858 <109360413+lolson5858@users.noreply.github.com>
2022-08-10 11:40:44 -0500

    first commit

---
+++ apps.js
@@ -0,0 +1,7 @@
+const greeting = "Good Morning Luke! Your inspirational poem of the day is.....";
+const firstLines = ["Behold, a being of true heart!", "Today is a good day to be your best self", "Blessed be the fruit"];
+const secondLines = ["one that fears no failure,", "FEAR NOT", "like a blackberry glittering in the sun"]
+const thirdLines = ["you shall prevail.", "for you are the chosen one.", "go kick life's ass."]
+
+
+console.log(greeting);
