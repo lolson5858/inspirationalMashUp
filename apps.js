8b5ebba87139600a540a1df0cba77466c5dbb22d
lolson5858 <109360413+lolson5858@users.noreply.github.com>
2022-08-10 13:31:07 -0500

    poem assembly in function

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
-const greeting = "Good Morning Luke! Your inspirational poem of the day is.....";
+const greeting = "Good Morning Luke! Your inspirational poem of the day is: ";
 const poemOb = {
-  firstLines:["Behold, a being of true heart!", "Today is a good day to be your best self", "Blessed be the fruit"],
-  secondLines: ["one that fears no failure,", "FEAR NOT", "like a blackberry glittering in the sun"],
+  firstLines:["Behold, a being of true heart! ", "Today is a good day to be your best self ", "Blessed be the fruit "],
+  secondLines: ["one that fears no failure,", "FEAR NOT ", "like a blackberry glittering in the sun "],
   thirdLines: ["you shall prevail.", "for you are the chosen one.", "go kick life's ass."]
 }
 
@@ -10,8 +10,8 @@ const mashup = () => {
   let firstLine = poemOb.firstLines[Math.floor(Math.random() * 3)];
   let secondLine = poemOb.secondLines[Math.floor(Math.random() * 3)];
   let thirdLine = poemOb.thirdLines[Math.floor(Math.random() * 3)];
-  poem.push(firstLine + secondLine + thirdLine);
-  return poem;
+  poem.push(`${firstLine} ${secondLine} ${thirdLine}`);
+  return greeting + poem;
   
 }
 
